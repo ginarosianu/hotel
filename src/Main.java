f7284c3e80f9f34168328b655c4c5768a8452a4e
gina <gina.rosianu@gmail.com>
2019-03-16 00:10:28 +0200

    MyHotel

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import Domain.RoomValidator;
+import Repository.RoomRepository;
+import Service.RoomService;
+import UI.Console;
+
+public class Main {
+
+    public static void main(String[] args) {
+        RoomValidator validator = new RoomValidator();
+        RoomRepository repository = new RoomRepository(validator);
+        RoomService room = new RoomService(repository);
+        Console console = new Console(room);
+
+        console.run();
+    }
+}
